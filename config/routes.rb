107c895695498236dd5ecfb7b487a8865080b5dd
administrator <admin@administrators-MacBook-Pro.local>
2020-04-11 14:40:24 +0900

    link_to,イメージ画像

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about' , to: "static_pages#about" , as: "static_pages_about"
+
+  get 'static_pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
